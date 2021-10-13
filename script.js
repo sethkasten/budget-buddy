b2cac43f151e3b5a01c3e4367f8b22e05a963b9c
Sara <seshe19@gmail.com>
2021-10-13 14:39:47 -0400

    budget-form-done

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 "use strict";
 //
+const budgetForm = document.querySelector(".budget-form");
+const remainingBalance = document.querySelector(".balance");
+const itemDescriptionForm = document.querySelector(".item-description-form");
+document.querySelector(".total").textContent = "$0.00";
+
+budgetForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let budget = document.querySelector("#budget").value;
+  document.querySelector(".total").textContent = document
+    .querySelector(".total")
+    .textContent.slice(1);
+  let total = parseInt(document.querySelector(".total").textContent);
+  let balance = budget - total;
+  document.querySelector(".total").textContent = `$${total}`;
+  remainingBalance.textContent = `$${balance}`;
+});
+console.log();
+
+itemDescriptionForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
