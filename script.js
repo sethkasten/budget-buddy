8b37cced5fea654591668bfea075c4aa877af5bb
sethkasten <kastens@umich.edu>
2021-10-14 10:43:32 -0400

    delete-button-functions

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 "use strict";
-//
+
 const budgetForm = document.querySelector(".budget-form");
 const remainingBalance = document.querySelector(".balance");
 const itemDescriptionForm = document.querySelector(".item-description-form");
 const itemTableBody = document.querySelector(".item-table-body");
 const items = [];
+const deleteButton = document.querySelector(".delete");
+const clearButton = document.querySelector(".clear");
 
-document.querySelector(".total").textContent = "$0.00";
-document.querySelector(".Bills").textContent = "$0.00";
-document.querySelector(".Clothing").textContent = "$0.00";
-document.querySelector(".Entertainment").textContent = "$0.00";
-document.querySelector(".Food").textContent = "$0.00";
+document.querySelector(".total").textContent = "$0";
+document.querySelector(".Bills").textContent = "$0";
+document.querySelector(".Clothing").textContent = "$0";
+document.querySelector(".Entertainment").textContent = "$0";
+document.querySelector(".Food").textContent = "$0";
 
 const updateBalance = () => {
   let budget = document.querySelector("#budget").value;
@@ -46,6 +48,7 @@ const calculateTotal = () => {
       .querySelector(td)
       .textContent.slice(1);
   }
+
   document.querySelector(".total").textContent =
     parseFloat(document.querySelector(".Bills").textContent) +
     parseFloat(document.querySelector(".Clothing").textContent) +
@@ -71,7 +74,6 @@ const calculateTotal = () => {
       amount = document.querySelector(".Food").textContent;
       td = ".Food";
     }
-
     document.querySelector(td).textContent = `$${amount}`;
   }
 };
@@ -93,7 +95,7 @@ itemDescriptionForm.addEventListener("submit", (e) => {
       const button = document.createElement("button");
       addedItem.append(button);
       button.textContent = "Delete";
-      button.classList.add("clear-delete");
+      button.classList.add("delete");
     }
   }
   //   grabs category we've selected
@@ -102,19 +104,32 @@ itemDescriptionForm.addEventListener("submit", (e) => {
   document.querySelector(`.${category}`).textContent = document
     .querySelector(`.${category}`)
     .textContent.slice(1);
-
   // sets variable amount to the value we have input
   let amount = parseFloat(document.querySelector("#amount").value);
-  console.log(amount);
   //   sets variable total to the integer found in the selected category
   let total = parseFloat(document.querySelector(`.${category}`).textContent);
-  console.log(total);
   //   sets variable balance equal to total + amount
   let balance = total + amount;
-  console.log(balance);
   //   puts balance into proper td in table
   document.querySelector(`.${category}`).textContent = `$${balance}`;
   calculateTotal();
   updateBalance();
   itemDescriptionForm.reset();
+});
+
+itemTableBody.addEventListener("click", (e) => {
+  if (e.target.classList.contains("delete")) {
+    let category = e.target.parentNode.parentNode.childNodes[1].textContent;
+    let amount = parseFloat(
+      e.target.parentNode.parentNode.childNodes[2].textContent.slice(1)
+    );
+    let newCategoryTotal = parseFloat(
+      document.querySelector(`.${category}`).textContent.slice(1)
+    );
+    newCategoryTotal -= amount;
+    document.querySelector(`.${category}`).textContent = `$${newCategoryTotal}`;
+    e.target.parentNode.parentNode.remove();
+    calculateTotal();
+    updateBalance();
+  }
 });
